377db0afdc97a2c71092fa8a603813b0cb8908fd
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-27 17:22:25 +0300

    11. Уроки React JS (соц. сеть, шаг 1, вёрстка, grid)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import './App.css'
-import Header from './Header'
-import Technologis from './Technologis'
+import logo from './images/logo.png'
+import banner from './images/banner.jpg'
 
 const App = () => {
 	return (
-		<div>
-			<Header />
-			<Technologis />
+		<div className='app-wrapper'>
+			<header className='header'>
+				<img src={logo} alt='logo' />
+			</header>
+			<nav className='nav'>
+				<div>
+					<a href='#profile'>Профиль</a>
+				</div>
+				<div>
+					<a href='#messages'>Сообщения</a>
+				</div>
+				<div>
+					<a href='#news'>Новости</a>
+				</div>
+				<div>
+					<a href='#music'>Музыка</a>
+				</div>
+				<div>
+					<a href='#settings'>Настройки</a>
+				</div>
+			</nav>
+			<div className='content'>
+				<div>
+					<img src={banner} alt='banner' className='banner-img' />
+				</div>
+				<div>ava + description</div>
+				<div>
+					My post
+					<div>New Post</div>
+				</div>
+				<div>
+					<div>post 1</div>
+					<div>post 2</div>
+				</div>
+			</div>
 		</div>
 	)
 }
