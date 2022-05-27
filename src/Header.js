12f1df57c9673f5fb81f1d4cd8540bafff2785f1
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-27 14:20:09 +0300

    10. Уроки React JS (default опасность плагин auto import)

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Header() {
 	return (
 		<div>
-			<a href='#google'>Home</a>
-			<a href='#google'>News Feed</a>
+			<a href='#google'>Home</a>-
+			<a href='#google'>News Feed</a>-
 			<a href='#google'>Messages</a>
 		</div>
 	)
